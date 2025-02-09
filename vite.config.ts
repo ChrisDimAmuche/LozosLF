d9062551717106a8ee4373bf0fac240cd5164f83
ChrisDimAmuche <chrisamuchedim@gmail.com>
2025-02-09 08:34:25 -0300

    removed all files and directories

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Change 'base' to '/' for custom domains
-export default defineConfig({
-  plugins: [react()],
-  base: './',  // Change to '/repo-name/' if not using a custom domain
-});
