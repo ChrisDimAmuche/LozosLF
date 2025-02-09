721446cdad99feb7d43c6060b29474f23ff2f175
ChrisDimAmuche <chrisamuchedim@gmail.com>
2025-02-09 09:28:41 -0300

    fix: update base URL for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LozosLF/',  // Match your GitHub repository name
+  base: '/',  // Using custom domain
   build: {
     assetsDir: 'assets',
     rollupOptions: {
