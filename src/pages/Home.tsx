032681fb67986e2d383730d3d632c8ecbd718363
ChrisDimAmuche <chrisamuchedim@gmail.com>
2025-02-10 03:24:15 -0300

    fix: remove hero section background gradient

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ const Home = () => {
             alt="Crypto background"
             className="w-full h-full object-cover opacity-20"
           />
-          <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-black"></div>
+          <div className="absolute inset-0"></div>
         </div>
         
         <div className="relative z-10 text-center px-3 sm:px-4 lg:px-6 max-w-6xl mx-auto">
